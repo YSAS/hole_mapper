672c70d405e4bcafdd25223e7e65a5a9930833e2
Jeb Bailey <baileyji@umich.edu>
2015-02-16 11:00:20 -0500

    Add new filters

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from copy import deepcopy
 KNOWN_SLITS=['180','125','95','75','58','45']
 KNOWN_FILTERS=['BK7', 'Mgb_O69',' CalRT_O41','HotJupiter','Mgb_Rev2', 'IanR',
               'Halpha_Li', 'IanR_O77_80','Mgb-Rev2','CaT-OneOrder',
-              'BStar_O80_87', 'BulgeGC_1','Mgb_FourOrders']
+              'BStar_O80_87', 'BulgeGC_1','Mgb_FourOrders','RRLyr_1','JS_dSph']
 
 def _load_configs():
     setupfiles=glob(CONFIGDEF_DIRECTORY()+'*.configdef')
