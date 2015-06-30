dfa3f477c0a88cd9abb7eeca894c936e0cd15173
Jeb Bailey <baileyji@umich.edu>
2015-06-29 23:46:34 -0400

    GUIDEACQ_PCT_R_OVERLAP_OK must be < = SIMULTANEOUS_PLUG_PCT_R_OVERLAP_OK due to edge case

--- dimensions.py
+++ dimensions.py
@@ -6,8 +6,14 @@ PLATE_TARGET_RADIUS_LIMIT=12.23 #14.25'
 
 GUIDE_EXCLUSION_D=0.7
 
+#- overlap implies holes must have a slight gab between them
+#of %*r_other e.g if other hole is 1" and pct is -.05 then must be
+# clearance of .05" between their edges. 
 SIMULTANEOUS_PLUG_PCT_R_OVERLAP_OK=-0.05
 
 DRILLABLE_PCT_R_OVERLAP_OK=.9
 
-GUIDEACQ_PCT_R_OVERLAP_OK=0.0
+#must be <= SIMULTANEOUS_PLUG_PCT_R_OVERLAP_OK
+GUIDEACQ_PCT_R_OVERLAP_OK=-0.05
+
+assert GUIDEACQ_PCT_R_OVERLAP_OK<=SIMULTANEOUS_PLUG_PCT_R_OVERLAP_OK
