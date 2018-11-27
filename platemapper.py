8e8e6f8d3d1bf1e81c6d55eb2ced24a769b08a94
Jeb Bailey <baileyji@umich.edu>
2018-11-27 04:51:52 -0300

    Redo platemapper interface

--- platemapper.py
+++ platemapper.py
@@ -9,11 +9,11 @@ import pathconf
 from dimensions import PLATE_RADIUS
 from ttkcalendar import date_time_picker
 from setup import get_all_setups
-import argparse
 from logger import getLogger
 import tkMessageBox
+import tkFileDialog
 import errors
-log=getLogger('platemapper')
+log=getLogger('fiber_assigner')
 
 
 class HoleInfoDialog:
@@ -28,7 +28,8 @@ class HoleInfoDialog:
             self.initializeSingle(holes[0])
             
     def initializeSelection(self, holes):
-        
+        #This definition runs if two or more holes are within close proximity.
+        #It then outputs all of their data
         self.dialog=Tkinter.Toplevel(self.parent)
         self.dialog.bind("<FocusOut>", self.defocusCallback)
         self.dialog.bind("<Destroy>", self.destroyCallback)
@@ -36,29 +37,43 @@ class HoleInfoDialog:
         for i,hole in enumerate(holes):
             
             #self.canvas.itemconfigure('.'+id, state=Tkinter.DISABLED)
-
-            lbl_str=' '.join(['{}={}'.format(k,v)
-                              for k,v in hole.info.iteritems()])
-
+            #lbl_str=' '.join(['{}={}'.format(k,v)
+            #for k,v in hole.info.iteritems()])
+            #This reorganizes the key/values to only include certain pairs
+            #and in a certain order
+            #ADD IN FIELD NAME INTO INTERESTING KEYS
+            interesting_keys = ('priority', 'field', 'dec', 'ra',
+                                'epoch', 'id', 'type')
+            lbl_str_sub = {k: hole.info[k] for k in interesting_keys if k in hole.info}
+            lbl_str_sorted = ['{}={}\n\n'.format(k,v) for k,v in
+                              sorted(lbl_str_sub.iteritems(), key=lambda x: x[0]
+                                     in interesting_keys and
+                                     interesting_keys.index(x[0]) or
+                                     len(interesting_keys)+1)]
+        
             def cmd():
                 self.close()
                 self.initializeSingle(hole)
-            item=Tkinter.Label(self.dialog, text=lbl_str)
-            item.grid(row=i,column=0)
+            
+            item=Tkinter.Label(self.dialog, text=''.join(map(str,lbl_str_sorted)))
+            item.grid(row=0,column=i)
             item=Tkinter.Button(self.dialog,text='Select', command=cmd)
-            item.grid(row=i,column=1)
-
+            item.grid(row=1,column=i)
+    
     def initializeSingle(self, hole):
-
+        #This definition runs if extra holes are not nearby
         #self.canvas.itemconfigure('.'+holeID,state=Tkinter.DISABLED)
         self.dialog=Tkinter.Toplevel(self.parent)
         self.dialog.bind("<FocusOut>", self.defocusCallback)
         self.dialog.bind("<Destroy>", self.destroyCallback)
         
-        
-        recs=['{}={}'.format(k,v) for k,v in hole.info.iteritems()]
-        
-        for txt in recs:
+        #This extracts the desired items from the hole dictionary
+        #For some reason the first key becomes the last so priority is listed first
+        interesting_keys = ('priority', 'field', 'dec', 'ra', 'epoch', 'id', 'type')
+        recs_sub = {k: hole.info[k] for k in interesting_keys if k in hole.info}
+        recs_sorted = ['{}={}'.format(k,v) for k,v in sorted(recs_sub.iteritems(),key=lambda x: x[0] in interesting_keys and interesting_keys.index(x[0]) or len(interesting_keys)+1)]
+        #recs=['{}={}'.format(k,v) for k,v in recs_sub.iteritems()]
+        for txt in recs_sorted:
             Tkinter.Label(self.dialog, text=txt).pack()
         
         Tkinter.Button(self.dialog,text='Done',command=self.ok).pack()
@@ -96,7 +111,6 @@ class App(Tkinter.Tk):
         self.manager=platemanager.Manager()
         self._initialize_main()
 #        self._initialize_projector()
-        self.setup_info_window()
     
     def _initialize_projector(self):
         # create a second window and make it cover the entire projector screen
@@ -133,13 +147,13 @@ class App(Tkinter.Tk):
     def _initialize_main(self):
         
         #Basic window stuff
-        swid=120
+        swid=515
         bhei=55
-        whei=935
+        whei=685
         chei=whei-bhei
         wwid=chei+swid
         self.geometry("%ix%i"%(wwid,whei))
-        self.title("Plate Mapper")
+        self.title("Selection Window")
         
         #The sidebar
         frame = Tkinter.Frame(self, width=swid, bd=0, bg=None)#None)
@@ -147,7 +161,7 @@ class App(Tkinter.Tk):
 
         #Info display
         frame2 = Tkinter.Frame(self, height=bhei, bd=0, bg=None)#None)
-        frame2.place(x=0,y=whei-45-1)
+        frame2.place(x=0,y=whei-300)
 
         #The canvas for drawing the plate        
         self.canvas=BetterCanvas.BetterCanvas(self, chei, chei,
@@ -158,24 +172,59 @@ class App(Tkinter.Tk):
         self.canvas.bind("<Button-1>", self.canvasclick)
 
         #Buttons
-        Tkinter.Button(frame, text="Open Picker",
+        Tkinter.Label(frame,text='Directory Selection',font=("Helvetica",
+                                                             16)).pack()
+        Tkinter.Button(frame, text="Select a Main Dir",
+                       command=self.preprepared_dir).pack()
+        Tkinter.Label(frame,text='').pack()
+        Tkinter.Label(frame,text='Indiv Subdir Selection',font=("Helvetica",
+                                                                16)).pack()
+        Tkinter.Button(frame, text="Select Run Parameters Dir",
+                       command=self.select_run_params).pack()
+        Tkinter.Button(frame, text="Select M2FS Parameters Dir",
+                        command=self.select_m2fs_params).pack()
+        Tkinter.Button(frame, text="Select Setup Dir",
+                        command=self.select_setup_dir).pack()
+        Tkinter.Button(frame, text="Select Output Dir",
+                        command=self.select_output_dir).pack()
+        Tkinter.Label(frame,text='').pack()
+        Tkinter.Label(frame,text='Program Tools',font=("Helvetica",
+                                                                16)).pack()
+        Tkinter.Button(frame, text="Selection Window",
                        command=self.setup_info_window).pack()
-        Tkinter.Button(frame, text="Make plug",
+        Tkinter.Button(frame, text="Save Assignment",
                        command=self.make_plug).pack()
-#        Tkinter.Button(frame, text="Toggle Conflicts",
-#                       command=self.toggle_conflicts).pack()
-#        self.show_conflicts=True
+        Tkinter.Button(frame, text="Quit", command=self.confirm_quit).pack()
+        self.show_conflicts=True
 
         #Info output
         self.info_str=Tkinter.StringVar(value=self.status_string())
         Tkinter.Label(frame2, textvariable=self.info_str).pack(anchor='w')
     
+        self.run_params_str=Tkinter.StringVar(value=pathconf.run_params_dir)
+        Tkinter.Label(frame2, text='Run Parameters Dir:').pack(anchor='w')
+        Tkinter.Label(frame2, textvariable=self.run_params_str).pack(anchor='w')
+
+        self.m2fs_params_str=Tkinter.StringVar(value=pathconf.m2fs_params_dir)
+        Tkinter.Label(frame2, text='M2FS Parameters Dir:').pack(anchor='w')
+        Tkinter.Label(frame2, textvariable=self.m2fs_params_str).pack(anchor='w')
+  
+        self.setups_str=Tkinter.StringVar(value=pathconf.setups_dir)
+        Tkinter.Label(frame2, text='Setups Dir:').pack(anchor='w')
+        Tkinter.Label(frame2, textvariable=self.setups_str).pack(anchor='w')
+  
+        self.output_str=Tkinter.StringVar(value=pathconf.output_dir)
+        Tkinter.Label(frame2, text='Output Dir:').pack(anchor='w')
+        Tkinter.Label(frame2, textvariable=self.output_str).pack(anchor='w')
+        self.show_paths()
+  
+  
     def status_string(self):
         return ''
     
     def canvasclick(self, event):
         #Get holes that are within a few pixels of the mouse position
-        region=2
+        region=.15
         items=self.canvas.find_overlapping(event.x-region,
                                            event.y-region,
                                            event.x+region,
@@ -189,16 +238,82 @@ class App(Tkinter.Tk):
             holes=self.manager.get_holes(holeIDs)
             HoleInfoDialog(self.parent, self.canvas, holes)
 
-#    def toggle_conflicts(self):
-#        self.show_conflicts=not self.show_conflicts
-#        self.show()
-
     def show(self):
+        """ Updates the canvas """
         self.canvas.clear()
-#        self.proj_can.clear()
         self.info_str.set(self.status_string())
         self.manager.draw(self.canvas)
-#        self.manager.draw_image(self.proj_can)
+    
+    def update_string(self, directory, dir_name):
+        """
+            Updates text for desired directory on the program display
+        """
+        if directory:
+            if os.path.exists(directory):
+                return(directory)
+            else:
+                return('Not Selected')
+        elif dir_name == 'output': #output always has a default directory
+            if os.path.exists(os.path.join(os.getcwd(),dir_name)+os.sep):
+                return(os.path.join(os.getcwd(),dir_name)+os.sep)
+            else:
+                return(os.path.join(os.getcwd())+os.sep)
+        else:
+            if os.path.exists(os.path.join(os.getcwd(),dir_name)+os.sep):
+                return(os.path.join(os.getcwd(),dir_name)+os.sep)
+            else:
+                return('Not Selected')
+
+    def show_paths(self):
+        """
+            Updates the text for program paths on the display
+        """
+        self.info_str.set(self.status_string())
+        self.run_params_str.set(self.update_string(pathconf.run_params_dir, 'Run_Params'))
+        self.m2fs_params_str.set(self.update_string(pathconf.m2fs_params_dir, 'M2FS_Params'))
+        self.setups_str.set(self.update_string(pathconf.setups_dir, 'setups'))
+        self.output_str.set(self.update_string(pathconf.output_dir, 'output'))
+
+    def preprepared_dir(self):
+        """
+        Allows the user to specify a directory containing the default
+        subdirectories (m2fs params, output, run_params, setups). If this is run
+        all user defined paths are reset automatically
+        """
+        pathconf.run_params_dir=None
+        pathconf.m2fs_params_dir=None
+        pathconf.setups_dir=None
+        pathconf.output_dir=None
+        tkMessageBox.showinfo('Select Preprepared Directory Location',
+                              'Directory must contain "M2FS_Params" (must contain configs), "output", "Run_Params" (must contain  "deadfibers.txt"), and "setups" subdirectories. The names given are the exact names searched for.\n\nNote that all subdirectory paths are reset by running this and must be manually re-entered if desired.')
+        path_preprepared_dir=tkFileDialog.askdirectory(initialdir='./')
+        if not path_preprepared_dir == '':
+            os.chdir(path_preprepared_dir) #Changes work directory to selected one
+        self.show_paths()
+
+    def select_run_params(self):
+        selected_dir1=tkFileDialog.askdirectory(initialdir='./')
+        if not selected_dir1 == '':
+            pathconf.run_params_dir = selected_dir1
+        self.show_paths()
+
+    def select_m2fs_params(self):
+        selected_dir2=tkFileDialog.askdirectory(initialdir='./')
+        if not selected_dir2 == '':
+            pathconf.m2fs_params_dir = selected_dir2
+        self.show_paths()
+
+    def select_setup_dir(self):
+        selected_dir3=tkFileDialog.askdirectory(initialdir='./')
+        if not selected_dir3 == '':
+            pathconf.setups_dir = selected_dir3
+        self.show_paths()
+
+    def select_output_dir(self):
+        selected_dir4=tkFileDialog.askdirectory(initialdir='./')
+        if not selected_dir4 == '':
+            pathconf.output_dir = selected_dir4
+        self.show_paths()
 
     def setup_info_window(self):
     
@@ -230,9 +345,11 @@ class App(Tkinter.Tk):
         
         tree.heading('#0',text='Name',
                      command=lambda: tree_col_sort(tree, False, col='#0'))
+        tree.column('#0', width=500)
         for c in cols:
             tree.heading(c,text=c,
                          command=lambda c=c: tree_col_sort(tree, False, col=c))
+            tree.column(c, width=55)
         
         for setup in get_all_setups():
             tree.insert('', 'end', setup.name, text=setup.name, tags=(),
@@ -253,22 +370,65 @@ class App(Tkinter.Tk):
     def make_plug(self):
         self.manager.save_plug_and_config(self.canvas)
 
+    def confirm_quit(self):
+        """
+        Allows the user to quit the program
+        """
+        quit_window=Tkinter.Toplevel()
+        quit_label=Tkinter.Label(quit_window,text="           Are you sure?           ")
+        quit_label.pack()
+        confirm=Tkinter.Button(quit_window,text='Yes, quit',command=self.actually_quit)
+        confirm.pack()
+        cancel=Tkinter.Button(quit_window,text='No, cancel',
+                              command=quit_window.destroy)
+        cancel.pack()
+
+    def actually_quit(self):
+        """Executes the quit (as there were issues doing this in line"""
+        quit()
 
 class PopupWindow(object):
-    def __init__(self, master, get=str, query="No query specified"):
+    def __init__(self, master, get=str, query="No query specified", premade='',
+                 optional_premade='', clear=0, optional=0):
+        self.optional = optional
         top=self.top=Tkinter.Toplevel(master)
         self.l=Tkinter.Label(top,text=query)
         self.l.pack()
         if get == str:
             self.e=Tkinter.Entry(top)
+            self.e.insert(0, premade) #Adds a premade entry if provided
             self.e.pack()
             self.value=''
-        self.b=Tkinter.Button(top,text='Ok',command=self.cleanup)
+            if optional: #Used for optional second entry line (like for highlight)
+                self.ol=Tkinter.Label(top,text='Optional: Enter Field Name')
+                self.ol.pack()
+                self.o=Tkinter.Entry(top)
+                self.o.insert(0, optional_premade)
+                self.o.pack()
+            self.optional_value=None
+        self.b=Tkinter.Button(top,text='Confirm',command=self.cleanup)
         self.b.pack()
-
+        if clear:
+            self.a=Tkinter.Button(top,text='Clear Previous Highlight',
+                                  command=self.clear)
+            self.a.pack()
+        self.c=Tkinter.Button(top,text='Cancel',command=self.cancel)
+        self.c.pack()
+    
+    def clear(self): #Clears last input (like for highlight feature)
+        self.value='clear_command'
+        self.top.destroy()
+    
     def cleanup(self):
         self.value=self.e.get()
+        if self.optional:
+            self.optional_value = self.o.get()
         self.top.destroy()
+    
+    def cancel(self): #Cancel without altering any features
+        self.value = 'cancel_command'
+        self.top.destroy()
+
 
 def parse_cl():
     parser = argparse.ArgumentParser(description='Quadrant merger',
@@ -284,5 +444,5 @@ if __name__ == "__main__":
     args=parse_cl()
     pathconf.ROOT=args.dir
     app = App(None)
-    app.title('Plate Mapper')
+    app.title('Fiber Assigner')
     app.mainloop()
